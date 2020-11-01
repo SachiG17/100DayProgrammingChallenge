595b2e4944f809fa954696c2504843051df34a9a
Sachin Ghanteppagol <39979061+Sachin-Ghanteppagol@users.noreply.github.com>
2020-11-01 11:23:10 +0530

    Add files via upload

---
+++ karnatakflag.py
@@ -0,0 +1,19 @@
+#dependencies
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patch
+
+ 
+# the colors in the triangle
+yellow=patch.Rectangle((0,5),width=10,height=4,facecolor='#ffff08')
+
+red=patch.Rectangle((0,1),width=10,height=4,facecolor='#ff0000')
+_,plot=plt.subplots()
+plot.add_patch(yellow)
+plot.add_patch(red)
+#plot.plot() #plotting 
+ 
+
+plt.title('Happy Karnataka Rajyotsava')
+plot.plot()
+plt.show()
